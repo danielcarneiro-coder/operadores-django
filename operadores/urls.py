7d4a18644881f99c689fa60509507e258f2f2d52
Daniel M Carneiro <danielcarneiro@dmcsystemsp.info>
2026-06-09 16:40:37 -0300

    Atualização das Views de Operações

--- operadores/urls.py
+++ operadores/urls.py
@@ -1,11 +1,14 @@
-from importlib.resources import path
-
 from django.urls import path
-from .views import home
+from .views import home, somar, subtrair, multiplicar, dividir
+
 
 app_name = 'operadores'
 
 urlpatterns = [
-    path('', home)
-]
+    path('', home, name='home'),
+    path('somar/<int:a>/<int:b>/', somar, name='somar'),
+    path('subtrair/<int:a>/<int:b>/', subtrair, name='subtrair'),
+    path('multiplicar/<int:a>/<int:b>/', multiplicar, name='multiplicacao'),
+    path('dividir/<int:a>/<int:b>/', dividir, name='divisao')
+ ]
 
--- operadores/views.py
+++ operadores/views.py
@@ -1,8 +1,24 @@
-from django.http import HttpRequest, HttpResponseNotAllowed, HttpResponseRedirect, HttpResponseNotFound, \
+from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed, HttpResponseRedirect, HttpResponseNotFound, \
     HttpResponseServerError
 from django.shortcuts import render
-
 
 def home(req: HttpRequest):
     if req.method == 'GET':
         return render(req, template_name =  'operadores/index.html')
+    return HttpResponseNotAllowed(['GET'])
+
+def somar(req: HttpRequest, a, b):
+    soma = a + b
+    return HttpResponse('<h1>A Soma e´:  {}</h1>'.format(soma))
+
+def subtrair(req: HttpRequest, a, b):
+    subtracao = a - b
+    return HttpResponse('<h1>A Subtração é:  {}</h1>'.format(subtracao))
+
+def multiplicar(req: HttpRequest, a, b):
+    multiplicacao = a * b
+    return HttpResponse('<h1>A Multiplicação é:  {}</h1>'.format(multiplicacao))
+
+def dividir(req: HttpRequest, a, b):
+    divisao = a / b
+    return HttpResponse('<h1>A Divisão é:  {}</h1>'.format(divisao))
